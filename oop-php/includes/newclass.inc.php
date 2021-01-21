e824bb778ecbc26f0a054ec2b123f4aaafaed4e8
Anoskh <kimhab.zeecode@gmail.com>
2021-01-21 12:32:57 +0700

    Create Classes in OOP PHP

---
+++ oop-php/includes/newclass.inc.php
@@ -0,0 +1,13 @@
+<?php
+    class NewClass {
+        // Properties and Methods 
+        public $fun = "Hello class php"; 
+    }
+
+    $object = new NewClass; 
+
+    // var_dump: https://www.php.net/manual/en/function.var-dump.php
+    var_dump($object); 
+
+    // Create Classes in OOP PHP 
+?>
